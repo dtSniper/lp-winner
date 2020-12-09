23a64420facf1738cee2589a803523989d5af3f2
Sascha Wendt <sw@zieh-fix.com>
2020-12-09 20:56:40 +0100

    hide config&includes

--- initiateScript.php
+++ initiateScript.php
@@ -13,7 +13,7 @@ $f3->set( "TEMP", "/tmp/" );
 $port = $f3->get( "PORT" );
 $f3->set( "FULL_URL", $f3->get( "SCHEME" ) . '://' . $f3->get( "HOST" ) . ($port && !in_array( $port, [80, 443] ) ? (':' . $port) : '') . $f3->get( "BASE" ) );
 
-$f3->config( 'config.cfg' );
+$f3->config( 'include/config.cfg' );
 $MYSQL = $f3->get( "MYSQL" );
 
 $db = new SQL( "mysql:host=$MYSQL[SERVER];port=$MYSQL[PORT];dbname=$MYSQL[DB]", "$MYSQL[USER]", "$MYSQL[PASSWORD]" );
